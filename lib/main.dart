c755b89a81d3620c1581050186f439c44beacf26
adScarX <aa.djellouli@esi-sba.dz>
2023-03-10 02:53:51 +0100

    firebase_config

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
